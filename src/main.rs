dad64d11183e00e1471c0e724b071bcd984ef618
Edgar Nova <ragnarok540@gmail.com>
2019-03-11 23:52:20 -0500

    map complete

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
+extern crate rand;
+
 use std::collections::HashMap;
+use rand::Rng;
 
 fn main() {
+
+  let mut rng = rand::thread_rng();
+  let string = "the theremin is theirs or is it not theirs";
+  let mut m = 0;
+  let mut n = 3;
+  let mut map = HashMap::new();
     
-    let string = "the theremin is theirs or is it not theirs";
-    let mut n = 0;
-    let mut m = 3;    
-    let mut map = HashMap::new();
+  while n < string.len() {
+
+    let vec = map.entry(&string[m..n]).or_insert(Vec::new());
+    vec.push(&string[n..n + 1]);
+    m += 1;
+    n += 1;
+
+  }
+
+  //println!("{:?}", map);
+  
+  m = 0;
+  n = 3;
+  
+  let current = &string[m..n];
+  //let result = current;
+  let posibilities = map.get(current).unwrap();
     
-    while m <= string.len() {	  
-	  let counter = map.entry(&string[n..m]).or_insert(0);
-	  *counter += 1;
-      n += 1;
-      m += 1;
-    }
-    
-    println!("{:?}", map);
-    
+  println!("{:?}", posibilities[rng.gen_range(0, posibilities.len())]);
+  println!("{:?}", posibilities[rng.gen_range(0, posibilities.len())]);
+  println!("{:?}", posibilities[rng.gen_range(0, posibilities.len())]);
+  println!("{:?}", posibilities[rng.gen_range(0, posibilities.len())]);
+
 }
